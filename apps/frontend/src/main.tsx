db9d768656e1b7a40c434c7e4c66528246620b75
sushanshakya77 <sushanshakya77@gmail.com>
2022-01-24 09:57:14 +0545

    feat: react-query added

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,25 +4,36 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './app/app';
 import './styles.css';
 
-const themeLight = createTheme({
+const theme = createTheme({
   palette: {
     background: {
       default: '#def2f1',
     },
+    // primary: {
+    //   main: '#000000',
+    // },
+  },
+  typography: {
+    fontFamily: 'Poppins, sans-serif',
   },
 });
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
       <RecoilRoot>
-        <ThemeProvider theme={themeLight}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+        </QueryClientProvider>
       </RecoilRoot>
     </BrowserRouter>
   </StrictMode>,

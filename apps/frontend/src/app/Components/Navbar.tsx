cfd9b9cb7eac9b189323ac74b82a3e3be07083a1
sushanshakya77 <sushanshakya77@gmail.com>
2022-02-26 14:20:51 +0545

    refactor: code refactored

--- apps/frontend/src/app/Components/Navbar.tsx
+++ apps/frontend/src/app/Components/Navbar.tsx
@@ -1,15 +1,26 @@
 import styled from '@emotion/styled';
 import {
   AccountCircle,
+  Book,
   CircleNotifications,
   ExitToApp,
+  Feed,
+  Logout,
+  PersonAdd,
 } from '@mui/icons-material';
 import {
   AppBar,
   Avatar,
   Button,
   Container,
+  Divider,
   IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
   Toolbar,
   Tooltip,
   Typography,
@@ -51,7 +62,14 @@ function Navbar() {
         navigate('/login');
       });
   };
-
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div>
       <StyledAppBar elevation={0} color="transparent">
@@ -64,24 +82,93 @@ function Navbar() {
             </Link>
 
             <div style={{ flexGrow: 1 }} />
+            <Tooltip title="Explore">
+              <Link to="/explore">
+                <IconButton color="inherit">
+                  <Feed />
+                </IconButton>
+              </Link>
+            </Tooltip>
+            <Tooltip title="Trips">
+              <Link to="/trips">
+                <IconButton color="inherit">
+                  <Book />
+                </IconButton>
+              </Link>
+            </Tooltip>
             <Tooltip title="Notifications">
               <IconButton color="inherit">
                 <CircleNotifications />
               </IconButton>
             </Tooltip>
             <Tooltip title="Account">
-              <Link to="/user/info">
-                <IconButton color="inherit">
-                  <AccountCircle />
-                </IconButton>
-              </Link>
-            </Tooltip>
-
-            <Tooltip title="Logout">
-              <IconButton color="inherit" onClick={handleLogout}>
-                <ExitToApp />
+              <IconButton
+                color="inherit"
+                onClick={handleClick}
+                size="small"
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+              >
+                <Avatar sx={{ width: 32, height: 32 }}></Avatar>
               </IconButton>
             </Tooltip>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  '&:before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <List>
+                <Link
+                  to="user/info"
+                  style={{ textDecoration: 'none', color: 'black' }}
+                >
+                  <ListItem button>
+                    <ListItemIcon>
+                      <Avatar />
+                    </ListItemIcon>
+                    <ListItemText primary="My Profile" />
+                  </ListItem>
+                </Link>
+                <Divider />
+                <ListItem button onClick={handleLogout}>
+                  <ListItemIcon>
+                    <Logout fontSize="medium" />
+                  </ListItemIcon>
+                  <ListItemText primary="Logout" />
+                </ListItem>
+              </List>
+            </Menu>
           </Toolbar>
         </Container>
       </StyledAppBar>

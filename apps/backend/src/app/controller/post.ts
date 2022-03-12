03e7162d4be6544e1810a27cb4a45cefe8b7beb4
sushanshakya77 <sushanshakya77@gmail.com>
2022-03-12 22:09:43 +0545

    refactor: code refactor

--- apps/backend/src/app/controller/post.ts
+++ apps/backend/src/app/controller/post.ts
@@ -84,19 +84,31 @@ export const getSinglePost = async (req, res) => {
   }
 };
 
+// export const getTimelinePosts = async (req, res) => {
+//   try {
+//     const currentUser = await User.findById(req.params.userId);
+//     const userPosts = await Post.find({ userId: currentUser._id });
+//     const friendPosts = await Promise.all(
+//       currentUser.followings.map((friendId) => {
+//         return Post.find({ userId: friendId });
+//       })
+//     );
+//     res.status(200).json(userPosts.concat(...friendPosts));
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// };
 export const getTimelinePosts = async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.params.userId);
-    const userPosts = await Post.find({ userId: currentUser._id });
-    const friendPosts = await Promise.all(
-      currentUser.followings.map((friendId) => {
-        return Post.find({ userId: friendId });
-      })
-    );
-    res.status(200).json(userPosts.concat(...friendPosts));
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Post.find()
+    .populate('postedBy', '_id name')
+    .populate('comments.postedBy', '_id name')
+    .sort('-createdAt')
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const getUserPosts = async (req, res) => {
--- apps/backend/src/app/model/postModel.ts
+++ apps/backend/src/app/model/postModel.ts
@@ -18,7 +18,7 @@ const postSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
-    destination: {
+    location: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Destination',
     },

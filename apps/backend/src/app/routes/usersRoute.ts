b17ebdbf3a3c009025cc5f2732debf5b5caf22ca
sushanshakya77 <sushanshakya77@gmail.com>
2022-07-12 10:30:31 +0545

    feat: added route for reset password

--- apps/backend/src/app/routes/usersRoute.ts
+++ apps/backend/src/app/routes/usersRoute.ts
@@ -5,6 +5,7 @@ import {
   logoutController,
   refreshTokenController,
   registerController,
+  resetPasswordController,
 } from '../controller/user';
 
 const router = express.Router();
@@ -13,6 +14,7 @@ router.post('/login', loginController);
 router.post('/register', registerController);
 router.get('/refreshtoken', refreshTokenController);
 router.post('/forgotPassword', forgotPasswordController);
+router.post('/resetPassword/:token', resetPasswordController);
 router.post('/logout', logoutController);
 
 export default router;
